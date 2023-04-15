c10a06639f6f6b0474f1787e1dc2996b4353ab6a
Aadarsh <aadarsh9811594152@gmail.com>
2023-04-15 14:59:41 +0530

    more job added

--- src/components/UI/Screen/HomeScreen/homescreen.js
+++ src/components/UI/Screen/HomeScreen/homescreen.js
@@ -18,7 +18,7 @@ const HomeScreen = (props) => {
   const navigate = useNavigate();
   const [homeScreenData, setHomeScreenData] = useState({
     search: "",
-    filter: "doctor",
+    filter: "",
   });
 
   const handleProfessionFilter = (e) => {
@@ -100,10 +100,68 @@ const HomeScreen = (props) => {
             onChange={handleProfessionFilter}
             value={homeScreenData.filter}
           >
-            <option value="doctor">Doctor</option>
-            <option value="plumber">Plumber</option>
-            <option value="teacher">Teacher</option>
-            <option value="electrician">Electrician</option>
+           <option value="">Search by Profession</option>
+                <option value="accountant">Accountant</option>
+                <option value="architect">Architect</option>
+                <option value="babysitter">Babysitter</option>
+                <option value="barber">Barber</option>
+                <option value="beautician">Beautician</option>
+                <option value="caterer">Caterer</option>
+                <option value="car wash">Car Wash</option>
+                <option value="carpenter">Carpenter</option>
+                <option value="cleaner">Cleaner</option>
+                <option value="coach">Coach</option>
+                <option value="consultant">Consultant</option>
+                <option value="contractor">Contractor</option>
+                <option value="cook">Cook</option>
+                <option value="counselor">Counselor</option>
+                <option value="delivery driver">Delivery Driver</option>
+                <option value="designer">Designer</option>
+                <option value="developer">Developer</option>
+                <option value="doctor">Doctor</option>
+                <option value="driver">Driver</option>
+                <option value="electrician">Electrician</option>
+                <option value="engineer">Engineer</option>
+                <option value="financial advisor">Financial Advisor</option>
+                <option value="fitness trainer">Fitness Trainer</option>
+                <option value="gardener">Gardener</option>
+                <option value="hairdresser">Hairdresser</option>
+                <option value="handyman">Handyman</option>
+                <option value="housekeeper">Housekeeper</option>
+                <option value="interpreter">Interpreter</option>
+                <option value="labour">Labour</option>
+                <option value="landscaper">Landscaper</option>
+                <option value="lawyer">Lawyer</option>
+                <option value="makeup artist">Makeup Artist</option>
+                <option value="massage therapist">Massage Therapist</option>
+                <option value="mechanic">Mechanic</option>
+                <option value="milk man">Milk man</option>
+                <option value="musician">Musician</option>
+                <option value="nanny">Nanny</option>
+                <option value="nutritionist">Nutritionist</option>
+                <option value="painter">Painter</option>
+                <option value="personal assistant">Personal Assistant</option>
+                <option value="pet groomer">Pet Groomer</option>
+                <option value="photographer">Photographer</option>
+                <option value="photographer">Photographer</option>
+                <option value="plasterer">Plasterer</option>
+                <option value="plumber">Plumber</option>
+                <option value="programmer">Programmer</option>
+                <option value="real estate agent">Real Estate Agent</option>
+                <option value="repairman">Repairman</option>
+                <option value="resturant owner">Resturant Owner</option>
+                <option value="salesperson">Salesperson</option>
+                <option value="security guard">Security Guard</option>
+                <option value="software engineer">Software Engineer</option>
+                <option value="student">Student</option>
+                <option value="tailor">Tailor</option>
+                <option value="teacher">Teacher</option>
+                <option value="translator">Translator</option>
+                <option value="tutor">Tutor</option>
+                <option value="waiter">Waiter</option>
+                <option value="web developer">Web Developer</option>
+                <option value="writer">Writer</option>
+                <option value="other">Other</option>
           </select>
           <div className="profile-pic">
             <ProfilePic loggedInUser={loggedInUser} />
--- src/components/registration/registrationpage.js
+++ src/components/registration/registrationpage.js
@@ -37,20 +37,6 @@ const RegistrationPage = () => {
     pic: "",
   });
 
-  // const [pic , setPic] = useState("");
-
-  // const handlePhotoUpload = async () => {
-  //   const imageRef = ref(storage, `uploads/images/${Date.now()}-${pic.name}`);
-  //   const uploadResult = await uploadBytes(imageRef, pic);
-  //   console.log(uploadResult);
-  // }
-
-  // const handlePhotoUpload = async () => {
-  //   const imageRef = ref(storage, `uploads/images/${Date.now()}-${data.pic.name}`);
-  //   const uploadResult = await uploadBytes(imageRef, data.pic);
-  //   console.log(uploadResult);
-  // };
-
   const writeUserData = async (data) => {
     try {
       const imageRef = ref(
@@ -160,14 +146,11 @@ const RegistrationPage = () => {
       alert("sign up Successful");
       writeUserData(data);
       createuser();
-      // handlePhotoUpload();
       navigate("/login");
     } else {
       alert("oops! something went wrong, please try again");
     }
   };
-
-  // const handlePhotoUpload =
 
   return (
     <div className="registration-page">
@@ -195,7 +178,6 @@ const RegistrationPage = () => {
                     setData({ ...data, pic: e.target.files[0] });
                   }}
                 />
-                {/* <button className="upload-button" onClick={handlePhotoUpload}>Upload</button> */}
               </label>
             </div>
             <div className="gender-container">
@@ -266,10 +248,68 @@ const RegistrationPage = () => {
                 value={data.profession}
                 required
               >
+                <option value="">Select Profession</option>
+                <option value="accountant">Accountant</option>
+                <option value="architect">Architect</option>
+                <option value="babysitter">Babysitter</option>
+                <option value="barber">Barber</option>
+                <option value="beautician">Beautician</option>
+                <option value="caterer">Caterer</option>
+                <option value="car wash">Car Wash</option>
+                <option value="carpenter">Carpenter</option>
+                <option value="cleaner">Cleaner</option>
+                <option value="coach">Coach</option>
+                <option value="consultant">Consultant</option>
+                <option value="contractor">Contractor</option>
+                <option value="cook">Cook</option>
+                <option value="counselor">Counselor</option>
+                <option value="delivery driver">Delivery Driver</option>
+                <option value="designer">Designer</option>
+                <option value="developer">Developer</option>
                 <option value="doctor">Doctor</option>
-                <option value="plumber">Plumber</option>
-                <option value="teacher">Teacher</option>
+                <option value="driver">Driver</option>
                 <option value="electrician">Electrician</option>
+                <option value="engineer">Engineer</option>
+                <option value="financial advisor">Financial Advisor</option>
+                <option value="fitness trainer">Fitness Trainer</option>
+                <option value="gardener">Gardener</option>
+                <option value="hairdresser">Hairdresser</option>
+                <option value="handyman">Handyman</option>
+                <option value="housekeeper">Housekeeper</option>
+                <option value="interpreter">Interpreter</option>
+                <option value="labour">Labour</option>
+                <option value="landscaper">Landscaper</option>
+                <option value="lawyer">Lawyer</option>
+                <option value="makeup artist">Makeup Artist</option>
+                <option value="massage therapist">Massage Therapist</option>
+                <option value="mechanic">Mechanic</option>
+                <option value="milk man">Milk man</option>
+                <option value="musician">Musician</option>
+                <option value="nanny">Nanny</option>
+                <option value="nutritionist">Nutritionist</option>
+                <option value="painter">Painter</option>
+                <option value="personal assistant">Personal Assistant</option>
+                <option value="pet groomer">Pet Groomer</option>
+                <option value="photographer">Photographer</option>
+                <option value="photographer">Photographer</option>
+                <option value="plasterer">Plasterer</option>
+                <option value="plumber">Plumber</option>
+                <option value="programmer">Programmer</option>
+                <option value="real estate agent">Real Estate Agent</option>
+                <option value="repairman">Repairman</option>
+                <option value="resturant owner">Resturant Owner</option>
+                <option value="salesperson">Salesperson</option>
+                <option value="security guard">Security Guard</option>
+                <option value="software engineer">Software Engineer</option>
+                <option value="student">Student</option>
+                <option value="tailor">Tailor</option>
+                <option value="teacher">Teacher</option>
+                <option value="translator">Translator</option>
+                <option value="tutor">Tutor</option>
+                <option value="waiter">Waiter</option>
+                <option value="web developer">Web Developer</option>
+                <option value="writer">Writer</option>
+                <option value="other">Other</option>
               </select>
             </div>
 
